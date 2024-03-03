911b33cbb74b6fcfadb502c5ec64f513192381d4
AkifiNail <nail.akifi1234@gmail.com>
2024-03-03 02:43:17 +0100

    bad version

--- OneDrive/Bureau/DW-PROJECTS/game4/game4-serpent-tp3/assets/js/script.js
+++ OneDrive/Bureau/DW-PROJECTS/game4/game4-serpent-tp3/assets/js/script.js
@@ -7,11 +7,21 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+let survivantMode = false;
+
+function getRandomColor() {
+  const red = getRandomInt(256);
+  const blue = getRandomInt(256);
+  const green = getRandomInt(256);
+  return "rgb(" + red + "," + green + "," + blue + ")";
+}
+
 function getRandomInt2(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 const oppositeDirections = [2, 3, 0, 1];
+let scoreContent = document.querySelector(".scorecontent");
 
 let canvas = document.getElementById("terrain");
 let ctx = canvas.getContext("2d");
@@ -45,8 +55,12 @@ class Terrain {
       this.sol[i][0] = 0;
     }
 
-    let rocher = 40;
+    let rocher = 30;
     let rocherValue = 2;
+
+    console.log(survivantMode);
+
+    console.log(rocher);
 
     for (let k = 0; k < rocher; k++) {
       let i = getRandomInt2(1, largeur - 1);
@@ -72,20 +86,19 @@ class Terrain {
     }
   }
   read(i, j) {
-    return this.sol[i][j];
+    if (i >= 0 && i < this.largeur && j >= 0 && j < this.hauteur) {
+      return this.sol[i][j];
+    } else {
+      return undefined;
+    }
   }
 
   write(i, j, value) {
-    for (let i = 0; i < this.largeur; i++) {
-      for (let j = 0; j < this.hauteur; j++) {
-        this.sol[i][j] = value;
-      }
-    }
+    this.sol[i][j] = value;
   }
 }
 
 const terrain = new Terrain(20, 20);
-console.log(terrain.read(9, 3));
 
 class Anneau {
   constructor(i, j, couleur) {
@@ -105,15 +118,28 @@ class Anneau {
     this.i += tab[d].di;
     this.j += tab[d].dj;
 
-    if (this.i < 0) {
-      this.i = terrain.largeur - 1;
-    } else if (this.i >= terrain.largeur) {
-      this.i = 0;
-    }
-    if (this.j < 0) {
-      this.j = terrain.hauteur - 1;
-    } else if (this.j >= terrain.hauteur) {
-      this.j = 0;
+    if (this === serpent1) {
+      if (this.i < 0) {
+        console.log("perdu");
+      } else if (this.i >= terrain.largeur) {
+        console.log("perdu");
+      }
+      if (this.j < 0) {
+        console.log("perdu");
+      } else if (this.j >= terrain.hauteur) {
+        console.log("perdu");
+      }
+    } else {
+      if (this.i < 0) {
+        this.i = terrain.largeur - 1;
+      } else if (this.i >= terrain.largeur) {
+        this.i = 0;
+      }
+      if (this.j < 0) {
+        this.j = terrain.hauteur - 1;
+      } else if (this.j >= terrain.hauteur) {
+        this.j = 0;
+      }
     }
   }
 
@@ -157,15 +183,55 @@ class Serpent {
   }
 
   move() {
+    let serpentBodyValue = 2;
+    let serpentHeadValue = 2;
+    let serpentEndValue = 1;
+    let foodValue = 3;
     for (let n = this.longueur - 1; n > 0; n--) {
-      this.anneaux[n].copie(this.anneaux[n - 1]);
+      if (this.anneaux[n]) {
+        let serpentBody = this.anneaux[n].copie(
+          new Anneau(
+            this.anneaux[n - 1].i,
+            this.anneaux[n - 1].j,
+            this.anneaux[n].couleur
+          )
+        );
+        serpentBody = serpentBodyValue;
+      }
     }
     let head = this.anneaux[0];
     let nextCellValue = head.read(this.direction);
-    console.log(nextCellValue);
+    let last = this.anneaux[this.anneaux.length - 1];
+
+    if (
+      last.i == 0 ||
+      last.i == terrain.largeur - 1 ||
+      last.j == 0 ||
+      last.j == terrain.hauteur - 1
+    ) {
+      serpentEndValue = 0;
+    }
 
     let randomNumber = getRandomInt(10);
     let newDirection = getRandomInt(4);
+
+    terrain.write(head.i, head.j, serpentHeadValue);
+    terrain.write(last.i, last.j, serpentEndValue);
+    terrain.write(food.i, food.j, foodValue);
+
+    if (nextCellValue === 3) {
+      this.extend();
+      food.place();
+      food.draw();
+      this.longueur++;
+      terrain.write(food.i, food.j, 3);
+      score++;
+      scoreContent.textContent = score;
+    }
+
+    if (this === serpent1 && nextCellValue === 2) {
+      console.log("ok");
+    }
 
     if (this === serpent1) {
       head.move(this.direction);
@@ -173,8 +239,8 @@ class Serpent {
     }
 
     if (
-      nextCellValue === 0 ||
       nextCellValue === 1 ||
+      nextCellValue === 0 ||
       nextCellValue === undefined
     ) {
       head.move(this.direction);
@@ -188,12 +254,12 @@ class Serpent {
     }
 
     if (nextCellValue === 0) {
-      console.log("mur");
+      // console.log("mur");
     } else if (nextCellValue === 1) {
-      console.log("air");
+      // console.log("air");
     } else if (nextCellValue === 2) {
       head.move((this.direction + 1) % 4);
-      console.log("roche");
+      // console.log("roche");
     }
   }
 
@@ -210,12 +276,36 @@ class Serpent {
   }
 }
 
+let score = 0;
+
+class Food {
+  constructor(i, j) {
+    this.i = i;
+    this.j = j;
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    let x = this.i * 20;
+    let y = this.j * 20;
+    ctx.fillRect(x, y, 20, 20);
+  }
+  place() {
+    let validLocation = false;
+    while (!validLocation) {
+      this.i = getRandomInt(terrain.largeur);
+      this.j = getRandomInt(terrain.hauteur);
+      if (terrain.read(this.i, this.j) === 1) {
+        validLocation = true;
+      }
+    }
+  }
+}
+
 // Création de plusieurs serpents
 let serpent1 = new Serpent(5, 10, 10, 0);
 let serpent2 = new Serpent(7, 15, 5, 2);
-let serpent3 = new Serpent(4, 8, 18, 1);
-
-serpent1.extend();
+let serpent3 = new Serpent(9, 5, 15, 1);
 
 function drawSnake() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -226,6 +316,17 @@ function drawSnake() {
   serpent1.draw();
   serpent2.draw();
   serpent3.draw();
+  key();
+}
+
+let food = new Food(5, 5);
+
+function drawSnake2() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  terrain.draw(ctx);
+  serpent1.move();
+  serpent1.draw();
+  food.draw();
   key();
 }
 
@@ -257,4 +358,17 @@ function toggleFullScreen() {
     document.exitFullscreen();
   }
 }
-setInterval(drawSnake, 100);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let classique = document.querySelector(".classique");
+  let survivant = document.querySelector(".survivant");
+
+  survivant.addEventListener("click", function () {
+    setInterval(drawSnake, 100);
+  });
+
+  classique.addEventListener("click", function () {
+    // survivantMode = true;
+    setInterval(drawSnake2, 100);
+  });
+});
